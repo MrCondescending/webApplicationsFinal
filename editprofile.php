42c3a2ea6c93dc49dc1ccaade72caf4ce1228554
MrCondescending <workmand1@mymail.nku.edu>
2019-04-22 17:26:14 -0400

    Added boilerplate editprofile page, and fixed js filepath issues.

--- editprofile.php
+++ editprofile.php
@@ -1,12 +1,3 @@
-<?php
-  include('config.php');
-
-  $username = $_SESSION['username'];
-  $user = new User($username, $database);
-  $_SESSION['user'] = $user;
-
-  $users_groups = getGroups($user->getUsername(), $database);
-?>
 <!doctype html>
 <html lang="en">
   <head>
@@ -52,47 +43,8 @@
         </form>
       </div>
     </nav>
-
     <main role="main">
-
-      <!-- Main jumbotron for a primary marketing message or call to action -->
-      <div class="jumbotron">
-        <div class="container">
-          <h1 class="display-3">Hello, <?php if($user->getFirstName() == ''){ echo $user->getUsername();}else{echo $user->getFirstName();}?>!</h1>
-          <p>Welcome back to the group calendar! Your account was created on: <?php echo substr($customer['created_date'], 0 , 10) ?></p>
-          <p><a class="btn btn-primary btn-lg" href="editprofile.php" role="button">Click here to edit your profile &raquo;</a></p>
-        </div>
-      </div>
-
-      <?php if($users_groups['groups'] != ''): ?>
-        <div class="container">
-          <!-- Example row of columns -->
-          <?php
-            $groups_arr = explode(';', $users_groups['groups']);?>
-
-
-            <div class="row">
-              <?php foreach($groups_arr as $group): ?>
-              <div class="col-md-4">
-                <h2><?php echo $group; ?></h2>
-                <p><?php echo getGroupDescription($group, $database); ?></p>
-                <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
-              </div>
-              <?php endforeach; ?>
-            </div>
-
-          <hr>
-
-        </div> <!-- /container -->
-      <?php else: ?>
-        <div class="row">
-          <div class="col-md-4">
-            <center><p>It seems that you have not added any groups yet! Click the "Groups" button on the top bar to search for groups to add!</p>
-          </div>
-        </div>
-      <?php endif;?>
     </main>
-
     <footer class="container">
       <p>&copy; Devin Workman 2019</p>
     </footer>
